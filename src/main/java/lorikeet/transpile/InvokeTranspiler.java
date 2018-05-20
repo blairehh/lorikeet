914901b287698704cfcdf5806b9f789c4a891e29
Blair Harris <blairehh@gmail.com>
2018-05-20 18:28:11 +0200

    add value transpiler

--- src/main/java/lorikeet/transpile/InvokeTranspiler.java
+++ src/main/java/lorikeet/transpile/InvokeTranspiler.java
@@ -5,57 +5,23 @@ import lorikeet.lang.Value;
 
 public class InvokeTranspiler {
 
+    private final ValueTranspiler value;
+
+    public InvokeTranspiler() {
+        this.value = new ValueTranspiler(this);
+    }
+
+    public InvokeTranspiler(ValueTranspiler value) {
+        this.value = value;
+    }
+
     public String transpile(Invoke invoke) {
         return String.format(
             "%s.%s(%s)",
-            this.transpileValue(invoke.getSubject()),
+            this.value.transpile(invoke.getSubject()),
             this.transpileFunctionName(invoke),
             this.transpileArguments(invoke)
         );
-    }
-
-    private String transpileValue(Value value) {
-        if (value instanceof Value.Variable) {
-            return this.transpileVar(value);
-        }
-        if (value instanceof Value.StrLiteral) {
-            return this.transpileStr(value);
-        }
-        if (value instanceof Value.BolLiteral) {
-            return this.transpileBol(value);
-        }
-        if (value instanceof Value.IntLiteral) {
-            return this.transpileInt(value);
-        }
-        if (value instanceof Value.DecLiteral) {
-            return this.transpileDec(value);
-        }
-        return "";
-    }
-
-    private String transpileVar(Value value) {
-        final Value.Variable var = (Value.Variable)value;
-        return String.format("v_%s", var.getName());
-    }
-
-    private String transpileStr(Value value) {
-        final Value.StrLiteral literal = (Value.StrLiteral)value;
-        return String.format("new lorikeet.core.Lk_struct_Str(\"%s\")", literal.getValue());
-    }
-
-    private String transpileBol(Value value) {
-        final Value.BolLiteral literal = (Value.BolLiteral)value;
-        return String.format("new lorikeet.core.Lk_struct_Bol(%s)", literal.getValue());
-    }
-
-    private String transpileInt(Value value) {
-        final Value.IntLiteral literal = (Value.IntLiteral)value;
-        return String.format("new lorikeet.core.Lk_struct_Int(%sL)", literal.getValue());
-    }
-
-    private String transpileDec(Value value) {
-        final Value.DecLiteral literal = (Value.DecLiteral)value;
-        return String.format("new lorikeet.core.Lk_struct_Dec(%s)", literal.getValue());
     }
 
     private String transpileFunctionName(Invoke invoke) {
@@ -65,7 +31,7 @@ public class InvokeTranspiler {
     private String transpileArguments(Invoke invoke) {
         StringBuilder builder = new StringBuilder();
         for (Value arg : invoke.getArguments()) {
-            builder.append(this.transpileValue(arg));
+            builder.append(this.value.transpile(arg));
             builder.append(", ");
         }
         if (builder.length() != 0) {
---
+++ src/test/java/lorikeet/Lang.java
@@ -0,0 +1,51 @@
+package lorikeet;
+
+import lorikeet.lang.Package;
+import lorikeet.lang.Type;
+import lorikeet.lang.SpecType;
+import lorikeet.lang.Expression;
+import lorikeet.lang.Value;
+import lorikeet.lang.Value.Variable;
+
+import java.util.List;
+import java.util.Arrays;
+
+public class Lang {
+
+    public static SpecType known(Type type) {
+        return new SpecType.Known(type);
+    }
+
+    public static Type type(String name, String... packages) {
+        return new Type(new Package(packages), name);
+    }
+
+    static Expression expression(Value value) {
+        return new Expression(Arrays.asList(value), value.getExpressionType().get());
+    }
+
+    public static Value literal(int number) {
+        return new Value.IntLiteral(String.valueOf(number));
+    }
+
+    public static Value literal(String value) {
+        return new Value.StrLiteral(value);
+    }
+
+    public static Value literal(double number) {
+        return new Value.DecLiteral(String.valueOf(number));
+    }
+
+    public static Value literal(boolean value) {
+        return new Value.BolLiteral(String.valueOf(value));
+    }
+
+    public static Value variable(boolean param, String name, SpecType type) {
+        return new Variable(param, name, type);
+    }
+
+    public static <A> List<A> listOf(A... values) {
+        return Arrays.asList(values);
+    }
+
+}
