94ff888a7fcf476cb14bbb5367d4d35bbe029948
Blair Harris <blairehh@gmail.com>
2019-04-18 14:53:06 +0200

    rename SunHttpServer

--- src/main/java/lorikeet/web/SunHttpServerEngine.java
+++ src/main/java/lorikeet/web/SunHttpServerEngine.java
@@ -8,11 +8,11 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
-public class SunHttpServer {
+public class SunHttpServerEngine {
 
     private final HttpServer server;
 
-    public SunHttpServer(WebServer webserver) throws IOException {
+    public SunHttpServerEngine(WebServer webserver) throws IOException {
         this.server = HttpServer.create(new InetSocketAddress(1111), 0);
         for (WebEndpoint webEndpoint : webserver.getRouter().getDispatcher().getEndpoints()) {
             this.server.createContext(webEndpoint.getPath(), new NoContentEndpoint());
