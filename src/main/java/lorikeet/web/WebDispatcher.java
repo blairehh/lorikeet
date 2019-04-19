ab6d2607b9c7110f0e280fa47c76ce593e879f0a
Blair Harris <blairehh@gmail.com>
2019-04-19 19:37:19 +0200

    refactor WebDispatcher

--- src/main/java/lorikeet/web/WebDispatcher.java
+++ src/main/java/lorikeet/web/WebDispatcher.java
@@ -9,7 +9,7 @@ public class WebDispatcher {
     private final Seq<WebEndpoint> endpoints;
 
     public WebDispatcher() {
-        this.path = "";
+        this.path = "/";
         this.endpoints = Seq.empty();
     }
 
@@ -19,26 +19,40 @@ public class WebDispatcher {
     }
 
     public WebDispatcher path(String path) {
-        if (!path.startsWith("/")) {
-            return new WebDispatcher(this.path + "/" + path, this.endpoints);
-        }
-        return new WebDispatcher(this.path + path, this.endpoints);
+        return new WebDispatcher(normalizePath(path), this.endpoints);
     }
 
     public WebDispatcher serve(String endpointPath, IncomingRequestHandler handler, HttpMethod... methods) {
         return this.register(Seq.of(methods), endpointPath, handler);
     }
 
+    public WebDispatcher get(IncomingRequestHandler handler) {
+        return this.register(Seq.of(HttpMethod.GET), "", handler);
+    }
+
     public WebDispatcher get(String endpointPath, IncomingRequestHandler handler) {
         return this.register(Seq.of(HttpMethod.GET), endpointPath, handler);
+    }
+
+    public WebDispatcher post(IncomingRequestHandler handler) {
+        return this.register(Seq.of(HttpMethod.POST), "", handler);
     }
 
     public WebDispatcher post(String endpointPath, IncomingRequestHandler handler) {
         return this.register(Seq.of(HttpMethod.POST), endpointPath, handler);
     }
 
+    public WebDispatcher put(IncomingRequestHandler handler) {
+        return this.register(Seq.of(HttpMethod.PUT), "", handler);
+    }
+
+
     public WebDispatcher put(String endpointPath, IncomingRequestHandler handler) {
         return this.register(Seq.of(HttpMethod.PUT), endpointPath, handler);
+    }
+
+    public WebDispatcher delete(IncomingRequestHandler handler) {
+        return this.register(Seq.of(HttpMethod.DELETE), "", handler);
     }
 
     public WebDispatcher delete(String endpointPath, IncomingRequestHandler handler) {
@@ -46,25 +60,33 @@ public class WebDispatcher {
     }
 
     private WebDispatcher register(Seq<HttpMethod> methods, String endpointPath, IncomingRequestHandler handler) {
-        final Seq<WebEndpoint> endpoints = methods.map(method -> new WebEndpoint(method, joinPath(this.path, endpointPath), handler));
+        final Seq<WebEndpoint> endpoints = methods.map(method -> new WebEndpoint(method, normalizePath(this.path, endpointPath), handler));
         return new WebDispatcher(this.path, this.endpoints.push(endpoints));
-    }
-
-    public WebDispatcher done() {
-        final int lastIndex = this.path.lastIndexOf('/');
-        if (lastIndex < 0) {
-            return this;
-        }
-        return new WebDispatcher(path.substring(0, lastIndex), this.endpoints);
     }
 
     public Seq<WebEndpoint> getEndpoints() {
         return this.endpoints;
     }
 
-    private static String joinPath(String a, String b) {
-        return !b.startsWith("/")
-            ? a + "/" + b
-            : a + b;
+    public static String normalizePath(String part1, String part2) {
+        final String endPart = part2.startsWith("/")
+            ? part2.substring(1)
+            : part2;
+
+        final String path =  part1 + endPart;
+        if (path.equals("/")) {
+            return path;
+        }
+        return path.endsWith("/") ? path.substring(0, path.length() - 1) : path;
+    }
+
+    private static String normalizePath(String path) {
+        final String start = path.startsWith("/")
+            ? ""
+            : "/";
+        final String end = path.endsWith("/")
+            ? ""
+            : "/";
+        return start + path + end;
     }
 }
--- src/test/java/lorikeet/web/WebDispatcherTest.java
+++ src/test/java/lorikeet/web/WebDispatcherTest.java
@@ -9,7 +9,7 @@ public class WebDispatcherTest {
     static IncomingRequestHandler endpoint = (request, response) -> null;
 
     @Test
-    public void testRootPath() {
+    public void testSingleLevel() {
         WebDispatcher dispatcher = new WebDispatcher()
             .get("foo", endpoint)
             .post("bar", endpoint);
@@ -42,8 +42,8 @@ public class WebDispatcherTest {
             .path("/bar")
                 .post("/baz", endpoint)
                 .put("baz2", endpoint)
-                .done()
-            .delete("abba", endpoint)
+            .path("/")
+                .delete("abba", endpoint)
             .path("/bam")
                 .get("/bom", endpoint);
 
@@ -56,4 +56,44 @@ public class WebDispatcherTest {
             new WebEndpoint(HttpMethod.GET,"/bam/bom", endpoint)
         );
     }
+
+    @Test
+    public void testDeepPath() {
+        WebDispatcher dispatcher = new WebDispatcher()
+            .get("foo", endpoint)
+            .path("/bar")
+                .post("/baz", endpoint)
+                .put("baz2", endpoint)
+            .path("/a/b/c/d/e/f/g/h/")
+                .get("1", endpoint)
+                .get("2", endpoint);
+
+        assertThat(dispatcher.getEndpoints()).hasSize(5);
+        assertThat(dispatcher.getEndpoints()).contains(
+            new WebEndpoint(HttpMethod.GET,"/foo", endpoint),
+            new WebEndpoint(HttpMethod.POST,"/bar/baz", endpoint),
+            new WebEndpoint(HttpMethod.PUT,"/bar/baz2", endpoint),
+            new WebEndpoint(HttpMethod.GET,"/a/b/c/d/e/f/g/h/1", endpoint),
+            new WebEndpoint(HttpMethod.GET,"/a/b/c/d/e/f/g/h/2", endpoint)
+        );
+    }
+
+    @Test
+    public void testRootPath() {
+        WebDispatcher dispatcher = new WebDispatcher()
+            .get("foo", endpoint)
+            .get(endpoint)
+            .path("/bar")
+                .put("/baz", endpoint)
+                .post(endpoint);
+
+        assertThat(dispatcher.getEndpoints()).hasSize(4);
+        assertThat(dispatcher.getEndpoints()).contains(
+            new WebEndpoint(HttpMethod.GET, "/foo", endpoint),
+            new WebEndpoint(HttpMethod.PUT, "/bar/baz", endpoint),
+            new WebEndpoint(HttpMethod.GET, "/", endpoint),
+            new WebEndpoint(HttpMethod.POST, "/bar", endpoint)
+        );
+    }
+
 }
