ab3bacd42b7b4294810c8d9ac208abce07f257ce
Blair Harris <blairehh@gmail.com>
2019-05-25 21:40:37 +0200

    remove old capability register stuff

--- src/main/java/lorikeet/tools/CapabilityRegistry.java
+++
@@ -1,7 +0,0 @@
-package lorikeet.tools;
-
-import lorikeet.Err;
-
-public interface CapabilityRegistry<Identifier, Ability, Context> {
-    Err<Ability> find(Identifier identifier, Context context);
-}
