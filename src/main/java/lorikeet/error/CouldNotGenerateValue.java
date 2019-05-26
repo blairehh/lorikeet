4f139766b09477c0cafa6656981a960145baca51
Blair Harris <blairehh@gmail.com>
2019-05-26 14:37:56 +0200

    add Generator

---
+++ src/main/java/lorikeet/error/CouldNotGenerateValue.java
@@ -0,0 +1,9 @@
+package lorikeet.error;
+
+public class CouldNotGenerateValue extends LorikeetException {
+
+    public CouldNotGenerateValue() {
+        super(CouldNotGenerateValue.class);
+    }
+
+}
