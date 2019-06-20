2ba6323d954f31e9ea09aa8a70a3222cd37cd106
Blair Harris <blairehh@gmail.com>
2019-06-20 22:03:58 +0200

    dont print cell graph

--- src/test/java/lorikeet/ecosphere/articletesting/graph/CellGraphNodeTranscriberTest.java
+++ src/test/java/lorikeet/ecosphere/articletesting/graph/CellGraphNodeTranscriberTest.java
@@ -17,7 +17,6 @@ public class CellGraphNodeTranscriberTest {
         User user = action.yield(new CreateUser(), "bob@gmail.com", "secret", Seq.of(1, 2));
 
         String transcript = transcriber.transcribe(action.getCellGraph().getRootNode());
-        System.out.println(transcript);
     }
 
 }
