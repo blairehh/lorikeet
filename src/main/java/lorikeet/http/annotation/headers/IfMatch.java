3745d17ea2047b05bd222df8a14a4e824d4395bb
Blair Harris <blairehh@gmail.com>
2020-04-13 07:18:41 +0200

    add header annotations

---
+++ src/main/java/lorikeet/http/annotation/headers/IfMatch.java
@@ -0,0 +1,9 @@
+package lorikeet.http.annotation.headers;
+
+import java.lang.annotation.Retention;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(value=RUNTIME)
+public @interface IfMatch {
+}
